4290f584a6f3821f1779597120270d1224f6c49c
Chandra Yuda Andika <chandra.yuda.andika@gmail.com>
2020-12-25 19:06:35 +0700

    add config to connect with oracle database

--- application/config/database.php
+++ application/config/database.php
@@ -73,13 +73,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'admin',
+// 	'password' => 'admin',
+// 	'database' => 'college_shop',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'admin',
-	'password' => 'admin',
-	'database' => 'college_shop',
-	'dbdriver' => 'mysqli',
+	'hostname' => '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.18.26)(PORT=1521))(CONNECT_DATA=(SID=XE)))',
+	'username' => 'raka',
+	'password' => 'adminaa',
+	'database' => 'raka',
+	'schema'   => 'RAKA',
+	'dbdriver' => 'oci8',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
@@ -94,3 +117,5 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+
